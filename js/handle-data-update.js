200f35e3ebfa61c92cc1f3bbf021153108eb8a39
Islam rabea <rabeaeslam56@gmail.com>
2025-05-15 07:41:56 +0300

    complete phases CRUD is update

--- js/handle-data-update.js
+++ js/handle-data-update.js
@@ -4,13 +4,30 @@ function handleDataUpdate(productId, products) {
   let productCategory = document.getElementById("category");
   let productPrice = document.getElementById("price");
 
-  let uploadImage = document.getElementById("upload-image");
+  let sectionUpdate = document.getElementById("update-section");
+  let uploadImage = sectionUpdate.querySelector("#upload-image");
   let img = uploadImage.querySelector("img");
   let p = uploadImage.querySelector("p");
 
   let product = products.find((ele) => (ele.id == productId ? ele : undefined));
 
+  if (product) {
+    productName.value = product.title;
+    productDescription.value = product.description;
+    productPrice.value = product.price;
+    productCategory.selectedIndex = [...productCategory.options].findIndex(
+      (option) => option.text === product.category
+    );
+    img.src = product.img;
+  }
+
   console.log(product);
+  img.style.display = "block";
+  p.style.display = "none";
+  img.style.cssText = `
+    width: 100%;
+    height: 100%
+  `;
 }
 
 export { handleDataUpdate };
